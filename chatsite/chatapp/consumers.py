0226d86cd85597596b3a28232db60e52112325d7
rrdesh10 <51774394+rrdesh10@users.noreply.github.com>
2023-09-28 21:58:12 +0530

    web socket

--- chatsite/chatapp/consumers.py
+++ chatsite/chatapp/consumers.py
@@ -6,7 +6,8 @@ import json
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
+        self.room_group_name = 'chat%s' % self.room_name
+        print(self.room_group_name)
 
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -15,8 +16,35 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-    async def disconnect(self):
+    async def disconnect(self, code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
+            
         )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data['message']
+        username = data['username']
+        room = data['room']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,{
+                'type':'chat message',
+                'message':message,
+                'username': username,
+                'room':room,
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+        room = event['room']
+
+        await self.send(text_data=json.dumps({
+            'message':message,
+            'username': username,
+            'room':room,
+        }))
